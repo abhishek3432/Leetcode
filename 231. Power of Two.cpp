8a005804d3aa0f560bd6908ec1bf75ce38bd8f88
git config --global user.name Abhisek <akt343243@gmail.com>
2023-10-27 08:13:12 +0530

    update

--- 231. Power of Two.cpp
+++ 231. Power of Two.cpp
@@ -1,11 +1,29 @@
+// class Solution {
+// public:
+//     bool isPowerOfTwo(int n) {
+//     for(int i=0;i<31;i++)
+//     {
+//         if(pow(2,i)==n)
+//             return true;
+//     }
+//     return false;
+//     }
+// };
+// updated
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-    for(int i=0;i<31;i++)
-    {
-        if(pow(2,i)==n)
-            return true;
-    }
-    return false;
+
+        if(n<=0)
+            return false;
+        int count=0;
+        while(n)
+        {
+            int bit=n&1;//get bit
+            if(bit)//check one and count++
+                count++;
+            n=n>>1;
+        }
+        return (count==1);
     }
 };
