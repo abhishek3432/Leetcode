8ff67ba9257712bcc9e4a1cc282cde6c202874d2
Abhishek Tiwari <akt343243@gmail.com>
2024-02-22 08:48:49 +0530

    commit

---
+++ Valid Anagram.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s = ''.join(sorted(s))
+        t= ''.join(sorted(t))
+        if(s==t):
+            return True
+        else :
+            return False
