937c499175b8c1c5d5904bc85941eba74e1da02f
Abhishek Tiwari <113165789+abhishek3432@users.noreply.github.com>
2024-04-04 18:20:25 +0530

    Time: 1 ms (16.35%) | Memory: 44.1 MB (96.97%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.java
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -1,13 +1,19 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        //| TC:(n)| SC:(n)
-       int [] ans= new int[2*n];
-       int k=0;
-       for(int i=0; i<n; i+=1)
+        //| TC:(n)| SC:(1)
+       for(int i=0; i<n; i++)
        {
-            ans[k++]=nums[i];
-            ans[k++]=nums[n+i];
+            nums[i] =nums[i]<<10;//shifting x 10bit left
+            nums[i] |=nums[n+i]; //storing x and y together
        }
-       return ans;
+       int j=2*n-1;
+       for(int i=n-1; i>=0; i--)
+       {
+            int y=nums[i]&(int)(Math.pow(2,10)-1);
+            int x=nums[i]>>10;
+            nums[j--]=y;
+            nums[j--]=x;
+       }
+       return nums;
     }
 }
