f2840d3c6b2bbc3de24abe5b968ab3ef8739c0d0
Abhishek Tiwari <113165789+abhishek3432@users.noreply.github.com>
2024-04-04 17:47:55 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (90.35%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.java
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -1,14 +1,12 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        //time complexity: O(n)
-        //space complexity: O(n)
-        int[] shuffleArray=new int[2*n];
-
-        for(int i=0; i<n; i++)
-        {
-            shuffleArray[2*i]=nums[i];
-            shuffleArray[2*i+1]=nums[n+i];
-        }
-        return shuffleArray;
+       int [] ans= new int[2*n];
+       int k=0;
+       for(int i=0; i<n; i+=1)
+       {
+            ans[k++]=nums[i];
+            ans[k++]=nums[n+i];
+       }
+       return ans;
     }
 }
