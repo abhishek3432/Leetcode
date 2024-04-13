deeb58356b69610ad9f9056f25fd8efcf96145c1
Abhishek Tiwari <113165789+abhishek3432@users.noreply.github.com>
2024-04-13 07:36:20 +0530

    Time: 1 ms (71.11%) | Memory: 46.2 MB (48.07%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,25 +1,25 @@
 class Solution {
     public int trap(int[] height) {
-        //Approach: Arraypreprocessing
-        //TC:(n)| SC:(n)
-        int[] maxLeft= new int[height.length];
-        maxLeft[0]=height[0];
-        int[] maxRight= new int[height.length];
-        maxRight[height.length-1]=height[height.length-1];
-        
-        for(int i=1; i<height.length; i++)
-            maxLeft[i]=Math.max(maxLeft[i-1],height[i]);
-                
-        for(int i=height.length-2; i>=0; i--)
-            maxRight[i]=Math.max(maxRight[i+1],height[i]);
-        
-        int water=0;
-        for(int i=0; i<height.length; i++)
-            water+=Math.min(maxLeft[i],maxRight[i]) - height[i];
-        
+        // Approach: Arraypreprocessing, create two array of same size holding max
+        // starting from left and right and then check which one is minimum and subtract
+        // with current height to get water stored
+        // TC:(n)| SC:(n)
+        int[] maxLeft = new int[height.length];
+        maxLeft[0] = height[0];
+        int[] maxRight = new int[height.length];
+        maxRight[height.length - 1] = height[height.length - 1];
+
+        for (int i = 1; i < height.length; i++)
+            maxLeft[i] = Math.max(maxLeft[i - 1], height[i]);
+
+        for (int i = height.length - 2; i >= 0; i--)
+            maxRight[i] = Math.max(maxRight[i + 1], height[i]);
+
+        int water = 0;
+        for (int i = 0; i < height.length; i++)
+            water += Math.min(maxLeft[i], maxRight[i]) - height[i];
+
         return water;
-        //Approach: use Two pointer Approach
-        //TC:(n)| SC:(1)
-        
+
     }
 }
