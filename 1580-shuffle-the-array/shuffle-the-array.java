932bf53f5c3651e176b8fb9baba5c44c709ee516
Abhishek Tiwari <113165789+abhishek3432@users.noreply.github.com>
2024-04-14 23:48:03 +0530

    Time: 1 ms (16.20%) | Memory: 44.3 MB (81.33%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.java
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -1,6 +1,17 @@
+//1470. Shuffle the Array
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        //| TC:(n)| SC:(1)
+    //Approach: Take new array and put given array value with logic
+    //TC:(n)| SC:(n)
+    //    int [] ans= new int[2*n];
+    //    int k=0;
+    //    for(int i=0; i<n; i+=1)
+    //    {
+    //         ans[k++]=nums[i];
+    //         ans[k++]=nums[n+i];
+    //    }
+    //    return ans;
+    //OptimizedApproach:TC:(n)| SC:(1)
        for(int i=0; i<n; i++)
        {
             nums[i] =nums[i]<<10;//shifting x 10bit left
