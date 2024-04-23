b7b49587c5d638468dfd697d779ad72c6864992b
Abhishek Tiwari <113165789+abhishek3432@users.noreply.github.com>
2024-04-23 23:15:19 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (25.26%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,9 +1,13 @@
+//27. Remove Element
 class Solution {
     public int removeElement(int[] nums, int val) {
+       //Approach:Use Two pointer and swap val to last value of array
+       //TC:(n)| SC:(1)
        int n=nums.length;
        int i=0;
        while(i<n)
        {
+            //check curval==val then swap reduce pointer to last second
             if(nums[i]==val)
             {
                 int swap=nums[i];
@@ -12,7 +16,7 @@ class Solution {
                 n--;
             }
             else
-                i++;
+                i++;//if not same move to next element
        }
        return i;
     }
