454e8108eb47449f3ac02221b972239d6760e583
Abhishek Tiwari <113165789+abhishek3432@users.noreply.github.com>
2024-04-24 23:31:56 +0530

    Time: 1 ms (15.77%) | Memory: 44.6 MB (35.04%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.java
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -1,30 +1,26 @@
 //1470. Shuffle the Array
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-    //Approach: Take new array and put given array value with logic
-    //TC:(n)| SC:(n)
-    //    int [] ans= new int[2*n];
-    //    int k=0;
-    //    for(int i=0; i<n; i+=1)
-    //    {
-    //         ans[k++]=nums[i];
-    //         ans[k++]=nums[n+i];
-    //    }
-    //    return ans;
-    //OptimizedApproach:TC:(n)| SC:(1)
-       for(int i=0; i<n; i++)
-       {
-            nums[i] =nums[i]<<10;//shifting x 10bit left
-            nums[i] |=nums[n+i]; //storing x and y together
-       }
-       int j=2*n-1;
-       for(int i=n-1; i>=0; i--)
-       {
-            int y=nums[i]&(int)(Math.pow(2,10)-1);
-            int x=nums[i]>>10;
-            nums[j--]=y;
-            nums[j--]=x;
-       }
-       return nums;
+        // Approach: Take given array and put two pointer at 0 and n position and create
+        // 2*n array and increament i+=2
+        // TC:(n)| SC:(n)
+        
+        // OptimizedApproach: Using Bitwise operator
+        // TC:(n)| SC:(1)
+        for (int i = 0; i < n; i++) {
+            // shifting x 10bit left
+            nums[i] = nums[i] << 10;
+            // storing x and y together
+            nums[i] |= nums[n + i];
+        }
+        // points end of arr
+        int j = 2 * n - 1;
+        for (int i = n - 1; i >= 0; i--) {
+            int y = nums[i] & (int) (Math.pow(2, 10) - 1);// get value 1 till 10 bit
+            int x = nums[i] >> 10;
+            nums[j--] = y;
+            nums[j--] = x;
+        }
+        return nums;
     }
 }
