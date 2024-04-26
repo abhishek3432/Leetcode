f5d70e65ba71b816b11ed22e137524ba2724ba2b
Abhishek Tiwari <113165789+abhishek3432@users.noreply.github.com>
2024-04-26 22:48:27 +0530

    Time: 1 ms (96.12%) | Memory: 45 MB (70.74%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.java
+++ 2058-concatenation-of-array/concatenation-of-array.java
@@ -1,16 +1,14 @@
+//1929. Concatenation of Array
 class Solution {
     public int[] getConcatenation(int[] nums) {
-        //BruteforceApproach: traverse whole array of length 2*n and put value at current index with some logic
-        //TC:(2*n)| SC:(n) 
- 
-       //OptimizedApproach: traverse only n time and put value at current index
-       //TC:(n)| SC:(n)
-        int n=nums.length;
-        int[] ans= new int[2*n];
-        for(int i=0; i<n; i++)
-        {
-            ans[i]=nums[i];
-            ans[n+i]=nums[i];
+        // BruteforceApproach: traverse whole array of length 2*n and put value at
+        // current index with some logic
+        // TC:(2*n)| SC:(n)
+        int n = nums.length;
+        int m = 2 * n;
+        int[] ans = new int[m];
+        for (int i = 0; i < m; i++) {
+            ans[i] = nums[i % n];
         }
 
         return ans;
