7564aa9dc1a03b7fb2f55abda8af994092991644
Abhishek Tiwari <113165789+abhishek3432@users.noreply.github.com>
2024-04-28 23:42:44 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (43.31%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        //Approach: Using Binary Search Algorithm
+        //TC:O(log n)| SC:O(1)
+        int l = 0;
+        int r = nums.length-1;
+        while(l<=r)
+        {
+            int m= l+(r-l)/2;
+            if(target>nums[m])
+                l=m+1;
+            else if(target<nums[m])
+                r = m-1;
+                else
+                    return m;
+        }
+        return -1;
+
+    }
+}
