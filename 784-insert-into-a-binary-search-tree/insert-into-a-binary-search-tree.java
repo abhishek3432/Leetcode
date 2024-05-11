2cee93a67a5bfd378f6dc1a37d2dc122559a6eb7
Abhishek Tiwari <113165789+abhishek3432@users.noreply.github.com>
2024-05-11 22:20:29 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (7.78%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.java
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.java
@@ -1,34 +1,49 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+//701. Insert into a Binary Search Tree
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
-        //Approach: Binary Search Algorithm to reach right position at leaf level 
-        // TC:O(log n)| SC:O(h)
-
-        if(root==null)
+         //Approach: Binary Search Algorithm to reach right position at leaf level using iteration
+        // TC:O(log n)| SC:O(1)
+        if (root==null)
+        {
             return new TreeNode(val);
-        
-        if(val>=root.val) //if value is equal or greater than node value
-            root.right= insertIntoBST(root.right, val);
-        else // if value is less than node value
-                root.left= insertIntoBST(root.left, val);
+        }
+        TreeNode newNode=new TreeNode(val);
+        TreeNode dummy= root;
+        while(dummy!=null)
+        {
+            if(newNode.val>dummy.val)
+            {
+                if(dummy.right==null)
+                {
+                    dummy.right=newNode;  
+                    break;
+                }
+                dummy=dummy.right;
+            }
+            else if (newNode.val<dummy.val)
+                    {
+                        if(dummy.left==null)
+                        {
+                            dummy.left=newNode;;  
+                            break;
+                        }
+                        dummy=dummy.left;
+                    }                
+        }
         return root;
 
-        
 
+        //Approach: Binary Search Algorithm to reach right 
+        //position at leaf level using recursion
+        // TC:O(log n)| SC:O(h)
+        // if(root==null)
+        //     return new TreeNode(val);
+        
+        // if(val > root.val) //if value is equal or greater than node value
+        //     root.right= insertIntoBST(root.right, val);
+        // else if(val < root.val)// if value is less than node value
+        //         root.left= insertIntoBST(root.left, val);
+        // return root;
 
     }
 }
