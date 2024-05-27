e17549206f49339783f7eb0e16f61b67a422da92
Abhishek Tiwari <113165789+abhishek3432@users.noreply.github.com>
2024-05-27 15:17:55 +0530

    Time: 3 ms (73.62%) | Memory: 42.5 MB (95.76%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,31 +1,35 @@
+//125. Valid Palindrome
 class Solution {
     public boolean isPalindrome(String s) {
-            
-        //TwoPointerApproach: use twopointer and some string and character methods| TC:(?)| SC:(?)
+        //Approach: use TwoPointer and some string and character methods
+        //TC:(n)| SC:(1)
         boolean palinString=true;
-        
-        int l=0;
-        int r=s.length()-1;
-        while(l<r)
+        int l=0;//left pointer
+        int r=s.length()-1;//right pointer
+        while(l<r)//check left not crossed right or vice versa
         {
-            Character lvalue= s.charAt(l);
-            Character rvalue= s.charAt(r);
+            Character lvalue= s.charAt(l);//get value at left pointer
+            Character rvalue= s.charAt(r);//get value at right pointer
 
-            if(!Character.isLetterOrDigit(lvalue))
-            {
+            if(!Character.isLetterOrDigit(lvalue))//check character is letter or digit
+            {//if not shift left pointer 
                 l++;
                 continue;
             }
-            if(!Character.isLetterOrDigit(rvalue))
-            {
+            if(!Character.isLetterOrDigit(rvalue))//check character is letter or digit
+            {//if not shift right pointer
                 r--;
                 continue;
             }
-            if(Character.toLowerCase(lvalue)!=Character.toLowerCase(rvalue))
-                palinString=false;
+            if(Character.toLowerCase(lvalue)!=Character.toLowerCase(rvalue))//get to 
+            //lowercase and check character are equal or not
+                return false;
+            //shift to pointer to check other character of string
             l++;
             r--;
         }
         return palinString;
     }
+    
+    
 }
